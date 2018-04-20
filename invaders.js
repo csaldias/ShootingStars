f2225adf135633bad72b25b687ba3d8159725d6a
Goferk <goferk95@gmail.com>
2018-04-20 15:22:26 -0300

    Agrandar espacio de movimiento de enemigos

--- invaders.js
+++ invaders.js
@@ -36,7 +36,7 @@ function create() {
   game.input.touch.preventDefault = false;
 
   music = game.add.audio('musicaFondo');
-
+  music.loopFull();
   music.play();
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -120,11 +120,11 @@ function createAliens () {
         }
     }
 
-    aliens.x = 100;
+    aliens.x = 50;
     aliens.y = 50;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+    var tween = game.add.tween(aliens).to( { x: 320 }, 900, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
